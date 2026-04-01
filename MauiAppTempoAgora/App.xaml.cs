38bb62863640763a603e2ed3ca07871f869a4837
Keila-dev <keilacristina.florindo@gmail.com>
2026-04-01 06:37:56 -0300

    Exibição de dados e mensagens de erros.

--- MauiAppTempoAgora/App.xaml.cs
+++ MauiAppTempoAgora/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace MauiAppTempoAgora
+﻿
+namespace MauiAppTempoAgora
 {
     public partial class App : Application
     {
@@ -8,5 +9,15 @@
 
             MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;//largura
+            window.Height = 600;//altura
+            return window;
+        }
+
     }
 }
